534afff4dd78ff92c64e9d9c0f4b963ebe41f5f5
Eduard-Mihai Burtescu <edy.burt@gmail.com>
2018-12-28 07:27:44 +0200

    rustc: remove `ty::print::FORCE_ABSOLUTE` altogether.

--- src/librustc_codegen_utils/symbol_names.rs
+++ src/librustc_codegen_utils/symbol_names.rs
@@ -223,11 +223,9 @@ fn get_symbol_hash<'a, 'tcx>(
 }
 
 fn def_symbol_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, def_id: DefId) -> ty::SymbolName {
-    ty::print::with_forced_absolute_paths(|| {
-        let mut cx = PrintCx::new(tcx, SymbolPath::new());
-        let _ = cx.print_def_path(def_id, None, Namespace::ValueNS, iter::empty());
-        cx.printer.into_interned()
-    })
+    let mut cx = PrintCx::new(tcx, SymbolPath::new());
+    let _ = cx.print_def_path(def_id, None, Namespace::ValueNS, iter::empty());
+    cx.printer.into_interned()
 }
 
 fn symbol_name<'a, 'tcx>(tcx: TyCtxt<'a, 'tcx, 'tcx>, instance: Instance<'tcx>) -> ty::SymbolName {
